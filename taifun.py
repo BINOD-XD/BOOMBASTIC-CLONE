cc06fc63c1f647f5cb908e58fc69239cbd0433be
REYAD X SHIPU <129636176+BINOD-XD@users.noreply.github.com>
2023-11-30 19:01:32 +0600

    Add files via upload

---
+++ taifun.py
@@ -0,0 +1,14 @@
+import os,platform
+try:os.system('git pull')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
+	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
+	import bombbro
+	bombbro.___rxs_xd__()
+elif bit == '32bit':
+	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
+	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT');time.sleep(1.0)
+	import bombbro
+	bombbro.___rxs_xd__()
