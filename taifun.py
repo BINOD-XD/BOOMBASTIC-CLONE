7032f9bc5bd631b32a8c87482c0587fe9f45a2e4
REYAD X SHIPU <129636176+BINOD-XD@users.noreply.github.com>
2023-11-30 20:06:16 +0600

    Update taifun.py

--- taifun.py
+++ taifun.py
@@ -5,10 +5,10 @@ bit = platform.architecture()[0]
 if bit == '64bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
-	import bombbro
-	bombbro.___rxs_xd__()
+	os.system('chmod 777 rxs')
+	os.system('./rxs')
 elif bit == '32bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT');time.sleep(1.0)
-	import bombbro
-	bombbro.___rxs_xd__()
+	os.system('chmod 777 rxs')
+	os.system('./rxs')
