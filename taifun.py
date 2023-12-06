2d038daec7bf4a9b91a296d7b600d12bd9d38325
REYAD X SHIPU <129636176+BINOD-XD@users.noreply.github.com>
2023-12-06 14:50:44 +0600

    Update taifun.py

--- taifun.py
+++ taifun.py
@@ -5,7 +5,7 @@ bit = platform.architecture()[0]
 if bit == '64bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
-	import run
+	import reyadshipu
 elif bit == '32bit':
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 32 BIT');time.sleep(1.0)
 	exit('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m 32 BIT WILL UPLOAD SOON !')
