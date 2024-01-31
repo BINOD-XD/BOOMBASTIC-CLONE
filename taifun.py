d993fd69da71f16018dee006d8894d2e235876c5
REYAD X SHIPU <129636176+BINOD-XD@users.noreply.github.com>
2024-02-01 01:45:03 +0600

    Update taifun.py

--- taifun.py
+++ taifun.py
@@ -3,10 +3,7 @@ try:os.system('git pull')
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
-	os.system('rm -rf BOOMBASTIC-CLONE')
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m UPDATING TOOL RXS !');time.sleep(1.0)
-	os.system('git clone https://github.com/BINOD-XD/BOOMBASTIC-CLONE > /dev/null 2>&1')
-	os.system('cd BOOMBASTIC-CLONE')
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;135;206;235m OPENING TOOL RXS !');time.sleep(1.0)
 	print('\033[1;92m[\033[1;36m✓\033[1;36m\033[1;92m] \033[38;2;0;255;127m YOUR DEVICE IS 64 BIT');time.sleep(1.0)
 	os.system('chmod +x ./wearerxsbro')
